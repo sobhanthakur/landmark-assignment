dec77d79ff4de6dcf8c7c89cf745a6e6bb24e61a
sobhanthakur <sobhant@mindfiresolutions.com>
2020-09-03 21:27:58 +0530

    Add Alerts

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import modelReducer from "./reducers/modelReducer";
+import alertReducer from "./reducers/alertReducer";
 
 export default combineReducers({
   modelReducer,
+  alertReducer
 });
--- client/src/redux/types.js
+++ client/src/redux/types.js
@@ -1,3 +1,6 @@
 export const LOAD_MODELS = "LOAD_MODELS";
 export const UPDATE_MODEL = "UPDATE_MODEL";
 export const ADD_MODEL = "ADD_MODEL";
+
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
