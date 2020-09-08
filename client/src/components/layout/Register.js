de421ced60e53a3694fc272bb092f059c46e13ef
sobhanthakur <sobhant@mindfiresolutions.com>
2020-09-08 14:03:05 +0530

    Remove unwanted images array from Register Component

--- client/src/components/layout/Register.js
+++ client/src/components/layout/Register.js
@@ -2,11 +2,9 @@ import React from "react";
 import ModelForm from "./ModelForm";
 
 const Register = () => {
-  const images = ["abc", "pqr"];
-
   return (
     <div className="container mt-5">  
-      <ModelForm images={images}></ModelForm>
+      <ModelForm></ModelForm>
     </div>
   );
 };
